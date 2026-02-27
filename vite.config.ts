4ca1bbda8b0ddab65a5051283637f9358175c5b7
Neel Patel <nilu.neelpatel@gmail.com>
2026-02-27 13:38:35 -0500

    cleared all

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // Replace 'your-repo-name' with the actual name of your GitHub repository
-  base: 'Neilpatel5502.github.io',
-})
